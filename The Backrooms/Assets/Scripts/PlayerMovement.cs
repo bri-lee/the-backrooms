47211324bdceadcace7b2348f8c8ad2870c943cd
SuyogKhanal5 <84912009+SuyogKhanal5@users.noreply.github.com>
2022-02-16 11:35:29 -0500

    Sprint Added

--- The Backrooms/Assets/Scripts/PlayerMovement.cs
+++ The Backrooms/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    public float moveSpeed = 1f;
+
+    public float sprintSpeed;
+
+    public CharacterController controller;
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal"); // Move along x axis
+        float z = Input.GetAxis("Vertical"); // Move along z axis
+
+        Vector3 movement = transform.right * x + transform.forward * z;
+
+        sprintSpeed = moveSpeed * 5;
+
+        if (Input.GetKey(KeyCode.LeftShift)) controller.Move(movement * sprintSpeed * Time.deltaTime);
+        else controller.Move(movement * moveSpeed * Time.deltaTime);
     }
 }
