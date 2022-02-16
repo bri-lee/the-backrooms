0cb9b6b725bec1061e328391cd3ca41064f09d65
SuyogKhanal5 <84912009+SuyogKhanal5@users.noreply.github.com>
2022-02-16 11:19:09 -0500

    Move Camera Left and Right

--- The Backrooms/Assets/Scripts/FirstPersonCamera.cs
+++ The Backrooms/Assets/Scripts/FirstPersonCamera.cs
@@ -6,11 +6,14 @@ public class FirstPersonCamera : MonoBehaviour
 {
     float xRotation;
     float mouseSens = 90f;
+    public Transform player;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Hide and lock cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -18,10 +21,14 @@ public class FirstPersonCamera : MonoBehaviour
     {
         // Mouse input
         float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSens;
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSens;
 
         // Rotate Camera up and down
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         transform.localEulerAngles = new Vector3(xRotation, 0f, 0f);
+
+        // Rotate Camera left and right
+        player.Rotate(Vector3.up * mouseX);
     }
 }
