d5867faac53d05ab8d31359c292a918941536573
SuyogKhanal5 <84912009+SuyogKhanal5@users.noreply.github.com>
2022-03-22 19:57:39 -0400

    Dialogue Disappears

--- The Backrooms/Assets/Scripts/TriggerHandler.cs
+++ The Backrooms/Assets/Scripts/TriggerHandler.cs
@@ -5,8 +5,10 @@ using UnityEngine.UI;
 
 public class TriggerHandler : MonoBehaviour
 {
-
     public Text DialogueBox;
+
+    [SerializeField]
+    public int timeForDialogue = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +18,28 @@ public class TriggerHandler : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Collided");
+
         if (other.CompareTag("Player"))
         {
             DialogueBox.enabled = true;
+
+            Debug.Log("Tag Compared");
+
+            // wait 3 seconds
+
+            StartCoroutine(Stopwatch(timeForDialogue));
+
+            Debug.Log("After Stopwatch");
         }
+    }
+
+    IEnumerator Stopwatch(int num)
+    {
+        yield return new WaitForSeconds(num);
+
+        DialogueBox.enabled = false;
+
+        Debug.Log("Inside IEnumerator");
     }
 }
