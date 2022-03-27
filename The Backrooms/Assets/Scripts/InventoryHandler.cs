baca0c576691b50c108d3adbca11ad96f326784b
SuyogKhanal5 <84912009+SuyogKhanal5@users.noreply.github.com>
2022-03-27 17:13:53 -0400

    InventoryHandler

---
+++ The Backrooms/Assets/Scripts/InventoryHandler.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryHandler : MonoBehaviour
+{
+    private Dictionary<InventoryItemData, InventoryItem> m_itemDictionary;
+    public List<InventoryItem> inventory { get; private set; }
+
+    private void Awake()
+    {
+        inventory = new List<InventoryItem>();
+        m_itemDictionary = new Dictionary<InventoryItemData, InventoryItem>();
+    }
+
+    public void Add(InventoryItemData referenceData)
+    {
+        if(m_itemDictionary.TryGetValue(referenceData, out InventoryItem value))
+        {
+            value.AddToStack();
+        }
+        else
+        {
+            InventoryItem newItem = new InventoryItem(referenceData);
+            inventory.Add(newItem);
+            m_itemDictionary.Add(referenceData, newItem);
+        }
+    }
+
+    public void Remove(InventoryItemData referenceData)
+    {
+        if (m_itemDictionary.TryGetValue(referenceData, out InventoryItem value))
+        {
+            value.RemoveFromStack();
+
+            if (value.stackSize == 0)
+            {
+                inventory.Remove(value);
+                m_itemDictionary.Remove(referenceData);
+            }
+        }
+    }
+
+}
+
+[Serializable]
+public class InventoryItem
+{ 
+    public InventoryItemData data { get; private set; }
+    public int stackSize { get; private set; }
+
+    public InventoryItem(InventoryItemData source)
+    {
+        data = source;
+        AddToStack();
+    }
+
+    public void AddToStack()
+    {
+        stackSize++;
+    }
+
+    public void RemoveFromStack()
+    {
+        stackSize--;
+    }
+}
