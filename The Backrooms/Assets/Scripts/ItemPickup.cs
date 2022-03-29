a494de4d5b518460032c64cba1671de1e31b8ba8
SuyogKhanal5 <84912009+SuyogKhanal5@users.noreply.github.com>
2022-03-29 19:37:56 -0400

    Fire Raycast to pick up item

--- The Backrooms/Assets/Scripts/ItemPickup.cs
+++ The Backrooms/Assets/Scripts/ItemPickup.cs
@@ -6,6 +6,7 @@ public class ItemPickup : MonoBehaviour
 {
     public InventoryItemData referenceItem;
     public GameObject item;
+    public GameObject player;
 
     public void OnHandlePickupItem()
     {
@@ -20,6 +21,32 @@ public class ItemPickup : MonoBehaviour
         if (other.tag == "Player")
         {
             OnHandlePickupItem();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        // get vector3 for where player is looking
+        Vector3 direction = Vector3.forward;
+
+        if (Input.GetKey("e"))
+        {
+            Debug.Log("e pressed");
+
+            if (Physics.Raycast(player.transform.position, player.transform.TransformDirection(direction), out hit, 8))
+            {
+                Debug.DrawRay(player.transform.position, player.transform.TransformDirection(direction) * hit.distance, Color.red);
+                Debug.Log("Hit object");
+
+                OnHandlePickupItem();
+            }
+            else
+            {
+                Debug.Log("Did not hit");
+            }
+
         }
     }
 }
